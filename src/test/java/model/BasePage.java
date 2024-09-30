9a47d8ff1c09918e08260dbba030eaf133897f73
Ina Ramankova <Romankova03071978@gmail.com>
2024-09-30 13:50:31 +0300

    Create testGoToHomePageByClickingOnScooterLogo() test in NavigationTest class; (#10)

--- src/test/java/model/BasePage.java
+++ src/test/java/model/BasePage.java
@@ -1,8 +1,12 @@
 package model;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public abstract class BasePage {
+
+    //Кнопка логотипа "Самокат"
+    private final By scooterLogoButton = By.cssSelector("[alt='Scooter']");
 
     private final WebDriver driver;
 
@@ -12,5 +16,15 @@ public abstract class BasePage {
 
     protected WebDriver getDriver() {
         return driver;
+    }
+
+    public HomePage clickScooterLogoButton() {
+        getDriver().findElement(scooterLogoButton).click();
+
+        return new HomePage(getDriver());
+    }
+
+    public String getCurrentURL() {
+        return getDriver().getCurrentUrl();
     }
 }
---
+++ src/test/java/model/OrderStatusPage.java
@@ -0,0 +1,10 @@
+package model;
+
+import org.openqa.selenium.WebDriver;
+
+public class OrderStatusPage extends BasePage {
+
+    public OrderStatusPage(WebDriver driver) {
+        super(driver);
+    }
+}
