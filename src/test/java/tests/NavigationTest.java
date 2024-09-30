c1201df2c61582a8279b18a500f8fe90a7789649
Ina Ramankova <romankova03071978@gmail.com>
2024-09-30 13:48:25 +0300

    Create testGoToHomePageByClickingOnScooterLogo() test in NavigationTest class; Create OrderStatusPage class; Add clickScooterLogoButton(), getCurrentURL() methods into BasePage class.

---
+++ src/test/java/tests/NavigationTest.java
@@ -0,0 +1,56 @@
+package tests;
+
+import model.BasePage;
+import model.FirstOrderFormPage;
+import model.HomePage;
+import model.OrderStatusPage;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import runner.BaseTest;
+
+@RunWith(Parameterized.class)
+public class NavigationTest extends BaseTest {
+
+    private final String urlAddress;
+    private final String pageName;
+    private BasePage page;
+
+    public NavigationTest(String urlAddress, String pageName) {
+        this.urlAddress = urlAddress;
+        this.pageName = pageName;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getURLData() {
+        return new Object[][]{
+                {"https://qa-scooter.praktikum-services.ru/", "HomePage"},
+                {"https://qa-scooter.praktikum-services.ru/order", "FirstOrderFormPage"},
+                {"https://qa-scooter.praktikum-services.ru/track?t=", "OrderStatusPage"}
+        };
+    }
+
+    @Test
+    public void testGoToHomePageByClickingOnScooterLogo() {
+        String expectedURL = "https://qa-scooter.praktikum-services.ru/";
+        getDriver().navigate().to(urlAddress);
+
+        switch (pageName) {
+            case "HomePage":
+                page = new HomePage(getDriver());
+                break;
+            case "FirstOrderFormPage":
+                page = new FirstOrderFormPage(getDriver());
+                break;
+            case "OrderStatusPage":
+                page = new OrderStatusPage(getDriver());
+                break;
+        }
+        String actualUrl = page
+                .clickScooterLogoButton()
+                .getCurrentURL();
+
+        Assert.assertEquals(expectedURL, actualUrl);
+    }
+}
