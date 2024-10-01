b780645a6f0c9e15979efd415b73d0045ea1dda0
Ina Ramankova <romankova03071978@gmail.com>
2024-10-01 13:40:21 +0300

    Create testGoToDzenByClickingYandexLogo() test in NavigationTest class. Add getCurrentPageInstance() method in NavigationTest class. Add clickYandexLogoButtonAndSwitchToYandexDzenPage() method in BaseProjectPage class. Create YandexDzenPage class extends BasePage. Create PageName enum.

--- src/test/java/pages/BaseProjectPage.java
+++ src/test/java/pages/BaseProjectPage.java
@@ -11,6 +11,9 @@ public abstract class BaseProjectPage extends BasePage {
     //Кнопка "Заказать" вверху страницы
     private final By topOrderButton = By.xpath("//div[@class='Header_Nav__AGCXC']/button[text()='Заказать']");
 
+    //Кнопка логотипа "Яндекс"
+    private final By yandexLogoButton = By.cssSelector("[alt='Yandex']");
+
     public BaseProjectPage(WebDriver driver) {
         super(driver);
     }
@@ -25,5 +28,15 @@ public abstract class BaseProjectPage extends BasePage {
         getDriver().findElement(topOrderButton).click();
 
         return new FirstOrderFormPage(getDriver());
+    }
+
+    public YandexDzenPage clickYandexLogoButtonAndSwitchToYandexDzenPage() {
+        getDriver().findElement(yandexLogoButton).click();
+
+        Object[] windowHandles = getDriver().getWindowHandles().toArray();
+        getDriver().switchTo().window((String) windowHandles[1]);
+        getDriver().navigate().refresh();
+
+        return new YandexDzenPage(getDriver());
     }
 }
---
+++ src/test/java/pages/YandexDzenPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class YandexDzenPage extends BasePage{
+
+    public YandexDzenPage(WebDriver driver) {
+        super(driver);
+    }
+}
