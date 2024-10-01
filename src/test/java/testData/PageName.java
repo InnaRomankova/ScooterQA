bcfac3bbb400b9dc0354cc65066c4904e78fd619
Ina Ramankova <Romankova03071978@gmail.com>
2024-10-01 13:50:03 +0300

    Create testGoToDzenByClickingYandexLogo() test in NavigationTest class. (#12)

---
+++ src/test/java/testData/PageName.java
@@ -0,0 +1,8 @@
+package testData;
+
+public enum PageName {
+
+    HOME_PAGE,
+    FIRST_ORDER_FORM_PAGE,
+    ORDER_STATUS_PAGE
+}
--- src/test/java/tests/NavigationTest.java
+++ src/test/java/tests/NavigationTest.java
@@ -9,15 +9,15 @@ import pages.FirstOrderFormPage;
 import pages.HomePage;
 import pages.OrderStatusPage;
 import runner.BaseTest;
+import testData.PageName;
 
 @RunWith(Parameterized.class)
 public class NavigationTest extends BaseTest {
 
     private final String urlAddress;
-    private final String pageName;
-    private BaseProjectPage page;
+    private final PageName pageName;
 
-    public NavigationTest(String urlAddress, String pageName) {
+    public NavigationTest(String urlAddress, PageName pageName) {
         this.urlAddress = urlAddress;
         this.pageName = pageName;
     }
@@ -25,10 +25,24 @@ public class NavigationTest extends BaseTest {
     @Parameterized.Parameters
     public static Object[][] getURLData() {
         return new Object[][]{
-                {"https://qa-scooter.praktikum-services.ru/", "HomePage"},
-                {"https://qa-scooter.praktikum-services.ru/order", "FirstOrderFormPage"},
-                {"https://qa-scooter.praktikum-services.ru/track?t=", "OrderStatusPage"}
+                {"https://qa-scooter.praktikum-services.ru/", PageName.HOME_PAGE},
+                {"https://qa-scooter.praktikum-services.ru/order", PageName.FIRST_ORDER_FORM_PAGE},
+                {"https://qa-scooter.praktikum-services.ru/track?t=", PageName.ORDER_STATUS_PAGE}
         };
+    }
+
+    public BaseProjectPage getCurrentPageInstance() {
+        switch (pageName) {
+            case HOME_PAGE:
+                return new HomePage(getDriver());
+
+            case FIRST_ORDER_FORM_PAGE:
+                return new FirstOrderFormPage(getDriver());
+
+            case ORDER_STATUS_PAGE:
+                return new OrderStatusPage(getDriver());
+        }
+        return null;
     }
 
     @Test
@@ -36,19 +50,20 @@ public class NavigationTest extends BaseTest {
         String expectedURL = "https://qa-scooter.praktikum-services.ru/";
         getDriver().navigate().to(urlAddress);
 
-        switch (pageName) {
-            case "HomePage":
-                page = new HomePage(getDriver());
-                break;
-            case "FirstOrderFormPage":
-                page = new FirstOrderFormPage(getDriver());
-                break;
-            case "OrderStatusPage":
-                page = new OrderStatusPage(getDriver());
-                break;
-        }
-        String actualUrl = page
+        String actualUrl = getCurrentPageInstance()
                 .clickScooterLogoButton()
+                .getCurrentURL();
+
+        Assert.assertEquals(expectedURL, actualUrl);
+    }
+
+    @Test
+    public void testGoToDzenByClickingYandexLogo() {
+        String expectedURL = "https://dzen.ru/?yredirect=true";
+        getDriver().navigate().to(urlAddress);
+
+        String actualUrl = getCurrentPageInstance()
+                .clickYandexLogoButtonAndSwitchToYandexDzenPage()
                 .getCurrentURL();
 
         Assert.assertEquals(expectedURL, actualUrl);
