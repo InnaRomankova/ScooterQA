af39ec284fcce4abe17a2d47d8e6141b1f53a60e
Ina Ramankova <Romankova03071978@gmail.com>
2024-10-03 00:56:02 +0300

    Create testGetErrorMessageInFirstNameField, testGetErrorMessageInLastNameField, testGetErrorMessageAddressField, testGetErrorMessageMetroStationField, testGetErrorMessagePhoneNumberField tests. (#15)

---
+++ src/test/java/testData/dataProvider/FirstScooterOrderFormParameters.java
@@ -0,0 +1,10 @@
+package testData.dataProvider;
+
+public class FirstScooterOrderFormParameters {
+
+    public static Object[][] provideFirstScooterOrderFormParameters() {
+        return new Object[][]{
+                {"Иван", "Иванов", "г.Москва, ул.Строителей, д.18, кв.6", "84999011234"}
+        };
+    }
+}
--- src/test/java/tests/BuyingScooterTest.java
+++ src/test/java/tests/BuyingScooterTest.java
@@ -11,7 +11,7 @@ import runner.BaseTest;
 import testData.MetroStation;
 import testData.RentalPeriod;
 import testData.ScooterColor;
-import testData.dataProvider.ScooterOrderFormParameters;
+import testData.dataProvider.*;
 import utils.TestUtils;
 
 @RunWith(JUnitParamsRunner.class)
@@ -49,5 +49,82 @@ public class BuyingScooterTest extends BaseTest {
                 .acceptOrderInDialogBox();
 
         Assert.assertTrue(confirmationList.getOrderConfirmationText().contains("Заказ оформлен"));
+    }
+
+    @Test
+    @Parameters(source = InvalidFirstNameParameters.class)
+    public void testGetErrorMessageInFirstNameField(String firstName) {
+        String expectedErrorMessage = "Введите корректное имя";
+
+        String actualErrorMessage = new HomePage(getDriver())
+                .clickAcceptCookieButton()
+                .clickBottomOrderButton()
+                .setFirstNameField(firstName)
+                .pressTabOnKeyboard()
+                .getFirstNameFieldErrorMessage();
+
+        Assert.assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+
+    @Test
+    @Parameters(source = InvalidLastNameParameters.class)
+    public void testGetErrorMessageInLastNameField(String lastName) {
+        String expectedErrorMessage = "Введите корректную фамилию";
+
+        String actualErrorMessage = new HomePage(getDriver())
+                .clickAcceptCookieButton()
+                .clickBottomOrderButton()
+                .setLastNameField(lastName)
+                .pressTabOnKeyboard()
+                .getLastNameFieldErrorMessage();
+
+        Assert.assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+
+    @Test
+    @Parameters(source = InvalidAddressParameters.class)
+    public void testGetErrorMessageAddressField(String address) {
+        String expectedErrorMessage = "Введите корректный адрес";
+
+        String actualErrorMessage = new HomePage(getDriver())
+                .clickAcceptCookieButton()
+                .clickBottomOrderButton()
+                .setOrderAddressField(address)
+                .pressTabOnKeyboard()
+                .getAddressFieldErrorMessage();
+
+        Assert.assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+
+    @Test
+    @Parameters(source = FirstScooterOrderFormParameters.class)
+    public void testGetErrorMessageMetroStationField(String firstName, String lastName, String address, String phoneNumber) {
+        String expectedErrorMessage = "Выберите станцию";
+
+        String actualErrorMessage = new HomePage(getDriver())
+                .clickAcceptCookieButton()
+                .clickBottomOrderButton()
+                .setFirstNameField(firstName)
+                .setLastNameField(lastName)
+                .setOrderAddressField(address)
+                .setPhoneNumberField(phoneNumber)
+                .clickNextButtonAndGetMetroStationFieldErrorMessage();
+
+        Assert.assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+
+    @Test
+    @Parameters(source = InvalidPhoneNumberParameters.class)
+    public void testGetErrorMessagePhoneNumberField(String phoneNumber) {
+        String expectedErrorMessage = "Введите корректный номер";
+
+        String actualErrorMessage = new HomePage(getDriver())
+                .clickAcceptCookieButton()
+                .clickBottomOrderButton()
+                .setPhoneNumberField(phoneNumber)
+                .pressTabOnKeyboard()
+                .getPhoneNumberFieldErrorMessage();
+
+        Assert.assertEquals(expectedErrorMessage, actualErrorMessage);
     }
 }
