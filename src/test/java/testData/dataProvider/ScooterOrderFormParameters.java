6fcee0841f77f0c208af07cb839fe1e46952697e
Ina Ramankova <Romankova03071978@gmail.com>
2024-10-02 14:13:17 +0300

    Implement JUnitParams dependency in the project. (#13)

---
+++ src/test/java/testData/dataProvider/ScooterOrderFormParameters.java
@@ -0,0 +1,17 @@
+package testData.dataProvider;
+
+import testData.MetroStation;
+import testData.RentalPeriod;
+import testData.ScooterColor;
+
+public class ScooterOrderFormParameters {
+
+    public static Object[][] provideScooterOrderFormParameters() {
+        return new Object[][]{
+                {"Иван", "Иванов", "г.Москва, ул.Строителей, д.18, кв.6", MetroStation.KRASNOSELSKAYA, "84999011234",
+                        "5", RentalPeriod.ONE_DAY, ScooterColor.BLACK_PEARL, "3-й этаж"},
+                {"Петр", "Петров", "г.Москва, ул.Новокузнецкая, 4-15", MetroStation.PREOBRAZHENSKAYA_SQUARE,
+                        "84999015555", "14", RentalPeriod.FIVE_DAYS, ScooterColor.GREY_HOPELESSNESS, "домофон не работает"}
+        };
+    }
+}
