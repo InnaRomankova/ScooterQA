2eb004a201728a1a667b3cea349cc1b6f5d3cb46
Ina Ramankova <romankova03071978@gmail.com>
2024-10-03 00:44:50 +0300

    Create testGetErrorMessageInFirstNameField(), testGetErrorMessageInLastNameField(), testGetErrorMessageAddressField(), testGetErrorMessageMetroStationField(), testGetErrorMessagePhoneNumberField() tests in BuyingScooterTest class. Add pressTabOnKeyboard(), getFirstNameFieldErrorMessage(), getLastNameFieldErrorMessage(), getAddressFieldErrorMessage(), clickNextButtonAndGetMetroStationFieldErrorMessage(), getPhoneNumberFieldErrorMessage() methods in FirstOrderFormPage class.  Create FirstScooterOrderFormParameters, InvalidAddressParameters, InvalidFirstNameParameters, InvalidLastNameParameters, InvalidPhoneNumberParameters classes in testData.dataProvider package.

--- src/test/java/pages/FirstOrderFormPage.java
+++ src/test/java/pages/FirstOrderFormPage.java
@@ -26,6 +26,20 @@ public class FirstOrderFormPage extends BaseProjectPage {
     //Кнопка "Далее"
     private final By nextButton = By.xpath("//button[text()='Далее']");
 
+    //Сообщение об ошибке для поля "Имя"
+    private final By firstNameFieldErrorMessage = By.cssSelector("[placeholder$='Имя']+div");
+
+    //Сообщение об ошибке для поля "Фамилия"
+    private final By lastNameFieldErrorMessage = By.cssSelector("[placeholder$='Фамилия']+div");
+
+    //Сообщение об ошибке для поля "Адрес: куда привезти заказ"
+    private final By orderAddressFieldErrorMessage = By.cssSelector("[placeholder*='Адрес']+div");
+
+    //Сообщение об ошибке для поля "Станция метро"
+    private final By metroStationFieldErrorMessage = By.cssSelector("[class^='Order_MetroError']");
+
+    //Сообщение об ошибке для поля "Телефон"
+    private final By phoneNumberFieldErrorMessage = By.cssSelector("[placeholder*='Телефон']+div");
 
     public FirstOrderFormPage(WebDriver driver) {
         super(driver);
@@ -71,5 +85,37 @@ public class FirstOrderFormPage extends BaseProjectPage {
         getDriver().findElement(nextButton).click();
 
         return new SecondOrderFormPage(getDriver());
+    }
+
+    public FirstOrderFormPage pressTabOnKeyboard() {
+
+        Actions actions = new Actions(getDriver());
+        actions
+                .sendKeys(Keys.TAB)
+                .perform();
+
+        return this;
+    }
+
+    public String getFirstNameFieldErrorMessage() {
+        return getDriver().findElement(firstNameFieldErrorMessage).getText();
+    }
+
+    public String getLastNameFieldErrorMessage() {
+        return getDriver().findElement(lastNameFieldErrorMessage).getText();
+    }
+
+    public String getAddressFieldErrorMessage() {
+        return getDriver().findElement(orderAddressFieldErrorMessage).getText();
+    }
+
+    public String clickNextButtonAndGetMetroStationFieldErrorMessage() {
+        getDriver().findElement(nextButton).click();
+
+        return getDriver().findElement(metroStationFieldErrorMessage).getText();
+    }
+
+    public String getPhoneNumberFieldErrorMessage() {
+        return getDriver().findElement(phoneNumberFieldErrorMessage).getText();
     }
 }
