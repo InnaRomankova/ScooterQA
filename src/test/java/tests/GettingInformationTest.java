d15f6fce4bf1e72832d62b8176d1a46b23b6dda4
Ina Ramankova <Romankova03071978@gmail.com>
2024-10-04 18:28:20 +0300

    Create testGetErrorMessageWhenClickOrderStatusButtonAndSetWrongOrderNumber(), testGetErrorMessageWhenClickLookButtonAndSetWrongOrderNumber() tests in GettingInformationTest class. (#16)

--- src/test/java/tests/GettingInformationTest.java
+++ src/test/java/tests/GettingInformationTest.java
@@ -1,14 +1,24 @@
 package tests;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.NoSuchElementException;
 import pages.HomePage;
+import pages.OrderStatusPage;
 import runner.BaseTest;
-import testData.HomePageData;
+import testData.*;
+import testData.dataProvider.URLDataParameters;
+import utils.TestUtils;
 
 import java.util.Map;
 
+@RunWith(JUnitParamsRunner.class)
 public class GettingInformationTest extends BaseTest {
+
+    final String wrongOrderNumber = "wrongOrderNumber_123";
 
     @Test
     public void testGetAnswersToImportantQuestions() {
@@ -19,5 +29,56 @@ public class GettingInformationTest extends BaseTest {
                 .getQuestionsAndAnswers(expectedAnswers.keySet());
 
         Assert.assertEquals(expectedAnswers, actualAnswers);
+    }
+
+    @Test
+    @Parameters(source = URLDataParameters.class)
+    public void testGetErrorMessageWhenClickOrderStatusButtonAndSetWrongOrderNumber(String urlAddress, PageName pageName) {
+        getDriver().navigate().to(urlAddress);
+
+        Boolean orderNotFoundPictureIsVisible = getCurrentPageInstance(pageName)
+                .clickOrderStatusButton(getCurrentPageInstance(pageName))
+                .setOderNumberIntoHeaderInputOderNumberField(getCurrentPageInstance(pageName), wrongOrderNumber)
+                .clickGoButton()
+                .verifyOrderNotFoundPictureIsVisible();
+
+        Assert.assertEquals(true, orderNotFoundPictureIsVisible);
+    }
+
+    @Test
+    public void testGetErrorMessageWhenClickLookButtonAndSetWrongOrderNumber() {
+        final String firstName = "Иван";
+        final String lastName = "Иванов";
+        final String address = "г.Москва, ул.Строителей, д.18, кв.6";
+        final MetroStation metroStation = MetroStation.KRASNOSELSKAYA;
+        final String phoneNumber = "84999011234";
+        final String deliveryDate = "5";
+        final RentalPeriod rentalPeriod = RentalPeriod.ONE_DAY;
+        final ScooterColor scooterColor = ScooterColor.BLACK_PEARL;
+        final String comment = "3-й этаж";
+
+        new HomePage(getDriver())
+                .clickAcceptCookieButton()
+                .clickBottomOrderButton();
+
+        TestUtils.fillScooterOrderForm(this, firstName, lastName, address,
+                        metroStation, phoneNumber, deliveryDate, rentalPeriod, scooterColor, comment)
+                .clickBottomOrderButton()
+                .acceptOrderInDialogBox()
+                .clickLookStatusButton();
+
+        try {
+            new OrderStatusPage(getDriver())
+                    .verifyOrderNotFoundPictureIsVisible();
+        } catch (NoSuchElementException e) {
+            System.out.println("Рисунок с сообщением \"Такого заказа нет\", как и ожидалось, отсутствует на странице");
+        }
+
+        Boolean orderNotFoundPictureIsVisible = new OrderStatusPage(getDriver())
+                .setInputFieldForLookButton(wrongOrderNumber)
+                .clickLookButton()
+                .verifyOrderNotFoundPictureIsVisible();
+
+        Assert.assertEquals(true, orderNotFoundPictureIsVisible);
     }
 }
--- src/test/java/tests/NavigationTest.java
+++ src/test/java/tests/NavigationTest.java
@@ -5,30 +5,12 @@ import junitparams.Parameters;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import pages.BaseProjectPage;
-import pages.FirstOrderFormPage;
-import pages.HomePage;
-import pages.OrderStatusPage;
 import runner.BaseTest;
 import testData.PageName;
 import testData.dataProvider.URLDataParameters;
 
 @RunWith(JUnitParamsRunner.class)
 public class NavigationTest extends BaseTest {
-
-    public BaseProjectPage getCurrentPageInstance(PageName pageName) {
-        switch (pageName) {
-            case HOME_PAGE:
-                return new HomePage(getDriver());
-
-            case FIRST_ORDER_FORM_PAGE:
-                return new FirstOrderFormPage(getDriver());
-
-            case ORDER_STATUS_PAGE:
-                return new OrderStatusPage(getDriver());
-        }
-        return null;
-    }
 
     @Test
     @Parameters(source = URLDataParameters.class)
